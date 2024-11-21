5b196558e1e5004ec22be4cb63c16633def984fe
Vinicius de Andrade Piovezan <vinicius.piovezan07@gmail.com>
2024-11-21 19:22:34 -0300

    first commit

---
+++ src/entities/Pedido.py
@@ -0,0 +1,28 @@
+class Pedido:
+    def __init__(self,id : int, itens : dict, status: int, price: int):
+        self._id = id
+        self._itens = itens
+        self._status = status
+        self._price = price
+    
+    @property
+    def id(self) -> int:
+        return self._id
+    
+    @property
+    def itens(self) -> dict:
+        return self._itens
+    
+    @property
+    def status(self) -> int:
+        return self._status
+    
+    @status.setter
+    def status(self, status) -> int:
+        self._status = status
+        
+    @property
+    def price(self) -> int:
+        return self._price
+        
+        
---
+++ src/usecases/PedidoUseCases.py
@@ -0,0 +1,23 @@
+
+
+class PedidosUseCases:
+    
+    @staticmethod
+    def createPedido():
+        pass
+    
+    @staticmethod
+    def requestPayment():
+        pass
+    
+    @staticmethod
+    def confirmPayment():
+        pass
+
+    @staticmethod
+    def requestProduction():
+        pass
+    
+    @staticmethod
+    def modifyStatus():
+        pass
